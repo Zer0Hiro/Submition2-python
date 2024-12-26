51fa8929e6661a28c21a9df9c81d6b72ea98eadf
Aliusblack2000 <aliusblack1999@gmail.com>
2024-12-26 19:17:39 +0200

    Update submition2v2.py

--- submition2v2.py
+++ submition2v2.py
@@ -1,1 +1,26 @@
+def belong(ls, num):
+    num in ls
+   
+def is_subject(ls_a, ls_b):
+    ls_a in ls_b
+    return True
+    ls_a not in ls_b
+    return False
+def are_equal(ls_a, ls_b):
+    ls_a in ls_b and ls_b in ls_a
+    return True
+    ls_a not in ls_a or ls_b not in ls_a
+    return False
+    
 
+
+def main():
+            
+    a = [12345]
+    b = [123456789]
+    c = [12345]
+    n = 4
+    print(belong(a, n))
+    print(is_subject(a, b))
+    print(are_equal(a, c))
+main()    
