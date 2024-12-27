10fb1eb931fbefa79fc8c4816e14ae40c4afc735
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2024-12-27 17:07:30 +0200

    Update submition2v2.py

--- submition2v2.py
+++ submition2v2.py
@@ -7,14 +7,19 @@ def is_subject(ls_a, ls_b):
 def are_equal(ls_a, ls_b):   
     return (ls_a in ls_b) and (ls_b in ls_a)
 
+def intersect(lstA,lstB):
+    int_lst = []
+    for i in range(len(lstA)):
+        for x in range(len(lstB)):
+            if lstA[i] == lstB[x]:
+                int_lst.append(lstB[x])
+    print(int_lst)
+    return int_lst
 
-def main():
-            
+def main():      
     a = [1,2,3,4,5]
-    b = [1,2,3,4,5,6,7,8,9]
+    b = [5,6,7,8,9]
     c = [1,2,3,4,5]
     n = 3
-    print(belong(a, n))
-    print(is_subject(a, b))
-    print(are_equal(a, c))
+    intersect(a,b)
 main()    
